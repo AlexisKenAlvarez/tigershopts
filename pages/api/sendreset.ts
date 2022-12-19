3ffb041b52777f609f873f568856159d877083f2
Alexis Ken Alvarez <alexisken1432@gmail.com>
2022-12-19 21:43:50 +0800

    TRYING TO FIX PRISMA pt2

--- pages/api/sendreset.ts
+++ pages/api/sendreset.ts
@@ -22,14 +22,14 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         })
 
         if (exists.length > 0) {
-            const user = await prisma.tokenresets.findMany({
+            const user = await prisma.tokenResets.findMany({
                 where: {
                     email: email
                 }
             })
 
             if (user.length > 0) {
-                const resetToken = await prisma.tokenresets.deleteMany({
+                const resetToken = await prisma.tokenResets.deleteMany({
                     where: {
                         email: email
                     }
@@ -40,7 +40,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
             const url = `${baseUrl}verified/${email}/${genToken}`
             await new Email(email, url).sendMagicLink()
 
-            const newToken = await prisma.tokenresets.create({
+            const newToken = await prisma.tokenResets.create({
                 data: {
                     email: email,
                     token: genToken
