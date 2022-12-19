4fae0f2c1853ceba364a5901906dd03a120929b6
Alexis Ken Alvarez <alexisken1432@gmail.com>
2022-12-19 12:43:26 +0800

    Added logout api

--- pages/api/logout.ts
+++ pages/api/logout.ts
@@ -1,8 +1,27 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { serialize } from 'cookie';
 
-export default (req:NextApiRequest, res:NextApiResponse) => {
+export default (req: NextApiRequest, res: NextApiResponse) => {
 
-  
+  const { cookies } = req
+
+  const jwt = cookies.authToken
+
+  if (!jwt) {
+    return res.json({ message: "Not logged in" })
+  } else {
+    const seralized = serialize("authToken", '', {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== 'development',
+      sameSite: "strict",
+      maxAge: 60 * 60 * 24 * 30,
+      path: '/'
+
+    })
+
+    res.setHeader("Set-Cookie", seralized)
+    res.status(200).json({ message: "Logout success", loggedOut: true })
+  }
 
 
 }
