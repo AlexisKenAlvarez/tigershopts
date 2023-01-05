50de2bf8838ab8282234b69e3bddce67593608ad
Alexis Ken Alvarez <alexisken1432@gmail.com>
2023-01-05 17:02:14 +0800

    added row span

--- components/admin/Products.tsx
+++ components/admin/Products.tsx
@@ -6,6 +6,7 @@ import { IoMdAddCircle } from 'react-icons/io'
 import { FaTrashAlt } from 'react-icons/fa'
 import { MdEdit } from 'react-icons/md'
 import { RiArrowDownSFill } from 'react-icons/ri'
+import { isTemplateSpan } from 'typescript';
 
 interface prod {
 
@@ -18,6 +19,16 @@ interface prod {
 
 const Products = ({ username, products }: { username: String, products: prod[] }) => {
     const router = useRouter()
+    const [span, setSpan] = useState<String>('')
+    const handleExpand = (name: String) => {
+        if (name === span) {
+            setSpan('')
+        } else{
+            setSpan(name)
+        }
+
+    }
+
     return (
 
         <>
@@ -26,29 +37,30 @@ const Products = ({ username, products }: { username: String, products: prod[] }
                 <h2 className="text-2xl font-bold text-greenSteps">Add product</h2>
             </a>
 
-            <div className="w-full sm:w-fit h-[70vh] mt-10 overflow-y-scroll grid sm:grid-cols-2 gap-y-6 sm:gap-y-10 sm:gap-x-10 pb-2 md:grid-cols-3 no-scrollbar xl:grid-cols-4 2xl:grid-cols-5 3xl:grid-cols-6">
+            <div className="w-full sm:w-fit h-[70vh] mt-10 overflow-y-scroll grid sm:grid-cols-2 gap-y-6 sm:gap-y-8 sm:gap-x-10 pb-2 md:grid-cols-3 no-scrollbar xl:grid-cols-4 2xl:grid-cols-5 3xl:grid-cols-6 transition-all ease-in-out select-none" style={{gridAutoRows: "minmax(20rem, 1fr)"}}>
                 {products.map((items) => {
                     return (
                         <>
-                            <div className='flex flex-col border-[1.5px] h-[19rem] w-[13rem] border-black rounded-2xl pb-3'>
-                                <div className='h-[12rem] flex justify-center items-center'>
-                                    <Image src={`/uploads/${items.image}`} alt={`${items.name}`} width="100" height="100" className='w-full h-full rounded-t-2xl'></Image>
+                            <div className='flex flex-col border-[1.5px] h-full w-[13rem] border-black rounded-2xl pb-3' style={span === items.name ? {gridRowStart: "span 2"} : {}}>
+                                <div className='h-[12rem] border-2 rounded-2xl'>
+                                    <Image src={`/uploads/${items.image}`} alt={`${items.name}`} width="100" height="100" className='w-full h-full rounded-t-2xl object-contain select-none'></Image>
                                 </div>
 
-                                <div className='flex flex-col h-[20rem] text-clip'>
-                                    <div className='w-full h-[3.4rem] flex items-center border-t-[1.5px] border-t-black justify-between'>
+                                <div className=' h-[7rem] text-clip'>
+                                    <div className='w-full h-[2.5rem] flex items-center border-t-[1.5px] border-t-black justify-between'>
                                         <p className='ml-3 text-greenBg font-semibold font-'>0 Favorites</p>
                                         <div className='flex items-center gap-x-2 mr-3'>
                                             <FaTrashAlt className='cursor-pointer'/>
                                             <MdEdit className='cursor-pointer'/>
                                         </div>
                                     </div>
-                                    <div className='w-[90%] mx-auto h-[2px] bg-black opacity-20'></div>
-                                    <div className='w-full h-full break-words px-3 pr-6 relative'>
-                                        <RiArrowDownSFill className='absolute top-2 right-3 cursor-pointer'/>
 
-                                        <p className=' leading-4 line-clamp-3 text-xs mt-2'>
-                                            <span className='text-greenBg font-black text-sm font-raleway'>{items.name}: </span>
+                                    <div className='w-[90%] mx-auto h-[2px] bg-black opacity-20'></div>
+                                    <div className='w-full h-full break-words px-3 pr-8 relative'>
+                                        <RiArrowDownSFill className='absolute top-2 right-3 cursor-pointer z-[5]' onClick={() => {handleExpand(items.name)}}/>
+
+                                        <p className={`text-xs mt-2 ${span === items.name ? 'line-clamp-none' : 'line-clamp-3'} select-none`}>
+                                            <span className='text-greenBg font-black text-sm font-raleway capitalize'>{items.name}: </span>
                                             {
                                                 items.desc
                                             }
--- components/admin/Reservations.tsx
+++ components/admin/Reservations.tsx
@@ -1,7 +1,11 @@
+import { useRef } from 'react'
+
 const Reservations = () => {
+
     return (
         <div className="text-2xl w-full h-full">
             Reservations
+            <input type="radio" value="HEY"></input>
         </div>
     );
 }
