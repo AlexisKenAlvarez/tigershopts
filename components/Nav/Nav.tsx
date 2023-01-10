b7c6061445bfdf30bef7537b022a92b6021890a0
Alexis Ken Alvarez <alexisken1432@gmail.com>
2023-01-10 14:38:11 +0800

    trying to make design pixel perfect

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -21,12 +21,12 @@ const Nav:FunctionComponent<myProp> = (props) => {
         <motion.nav initial={{ opacity: 0, y: -50 }} animate={{opacity: 100, y: 0}} transition={{duration: 0.7}}>
             <div className="w-full h-[6rem] border-b-[1px] shadow-md flex items-center relative z-40 bg-white">
                 <div className=" w-full max-w-[1600px] mx-auto flex justify-between items-center">
-                    <div className="flex items-center cursor-pointer md:ml-14 ml-8">
-                        <Image src="/logo.webp" alt="Logo" width='200' height='20' className="w-12 mx-auto" unoptimized={true}></Image>
+                    <div className="flex items-center cursor-pointer md:ml-14 lg:ml-36 ml-8">
+                        <Image src="/logo.webp" alt="Logo" width='200' height='20' className="w-10 mx-auto" unoptimized={true}></Image>
                         <h1 className="ml-2 text-greenSteps uppercase font-bold font-poppins text-lg">Tigershop</h1>
                     </div>
 
-                    <div className="md:block hidden mr-14">
+                    <div className="md:block hidden md:mr-14 lg:mr-36">
                         <NavItems status={status}/>
                     </div>
 
