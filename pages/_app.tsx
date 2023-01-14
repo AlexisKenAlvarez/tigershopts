7d77d13e1cbde1fe28541c5c5e9bc13b106d7832
Alexis Ken Alvarez <alexisken1432@gmail.com>
2023-01-14 15:43:29 +0800

    added nprogress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { AnimatePresence, motion } from 'framer-motion'
-import { useRouter } from 'next/router';
+import Router, { useRouter } from 'next/router';
 import Loader from '../components/Hero/Loader';
 import { useState, useEffect } from 'react'
+import Head from 'next/head';
+import NProgress from "nprogress"
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter()
@@ -26,8 +28,24 @@ export default function App({ Component, pageProps }: AppProps) {
     }, 3000);
   }
 
+  NProgress.configure({ showSpinner: false });
+
+  Router.events.on("routeChangeStart", (url) => {
+    console.log("Route is changing")
+    NProgress.start()
+  })
+
+  Router.events.on("routeChangeComplete", (url) => {
+    console.log("Route is done changing")
+    NProgress.done()
+
+  })
+
   return (
     <>
+      <Head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css" integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+      </Head>
       <div className={`${scroll ? 'overflow-auto w-full h-auto bg-greenBg' : 'overflow-hidden h-screen bg-greenBg'}`}>
 
         <AnimatePresence mode="wait">
