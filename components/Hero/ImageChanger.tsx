08104218780bf5625d7aa0836d2a2e6392495b47
Alexis Ken Alvarez <alexisken1432@gmail.com>
2023-01-17 01:01:32 +0800

    removed shadow

--- components/Hero/ImageChanger.tsx
+++ components/Hero/ImageChanger.tsx
@@ -10,7 +10,7 @@ interface prop {
 const ImageChanger: FunctionComponent<prop> = (props) => {
     const { image } = props
     return (
-        <motion.div initial={{opacity: "0%", x: 20}} animate={{opacity: "100%", x: 0}} transition={{duration: 0.8}} exit={{opacity: 0}} className="opacity-0 shadow-imageInset w-full h-full">
+        <motion.div initial={{opacity: "0%", x: 20}} animate={{opacity: "100%", x: 0}} transition={{duration: 0.8}} exit={{opacity: 0}} className="opacity-0 w-full h-full">
             <Image src={image} alt="Phone" className="z-[-1] md:opacity-100 opacity-50 w-full h-auto md:absolute md:top-[4rem] md:bottom-0 md:my-auto object-cover hidden md:block" fill />
         </motion.div>
     );
