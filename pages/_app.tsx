ca9b133ad56cefdc30ccff12af874136e0b54e2c
Alexis Ken Alvarez <alexisken1432@gmail.com>
2023-01-20 00:22:59 +0800

    still debugging

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -42,13 +42,13 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css" integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+        {/* <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css" integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ==" crossOrigin="anonymous" referrerPolicy="no-referrer" /> */}
       </Head>
-      <div className={`${scroll ? 'overflow-auto  w-full h-auto bg-greenBg' : 'overflow-hidden h-screen bg-greenBg'}`} >
+      <div className={`${scroll ? 'overflow-auto w-full bg-greenBg' : 'overflow-hidden bg-greenBg'}`} >
 
         <AnimatePresence mode="wait">
 
-          <motion.div className="overflow-x-hidden bg-topog dark:bg-black" initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.5 }} key={router.route}>
+          <motion.div className="overflow-x-hidden bg-topog dark:bg-black h-[100vh]" initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.5 }} key={router.route}>
 
             {active ? <Loader key="LOADER" /> : <Component {...pageProps} key="component" />}
 
